d054cd724e761f61141b2485ab7c6da5dfc3dab7
pnarguenes <132105205+pnarguenes@users.noreply.github.com>
2023-11-12 14:29:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="mathquiz",
+    version="0.1",
+    author="DSSS",
+    author_email="pnar.guenes@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy"],
+)
 
